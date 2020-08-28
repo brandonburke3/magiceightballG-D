bbd5b8c8d76ae312704b8ed51c35f2829b590328
Brandon Burke <65859226+brandonburke3@users.noreply.github.com>
2020-08-28 10:13:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,33 @@
-//switch statement version
-let userName = "Galactic Overlord";
-userName ? console.log('Hello, ' + userName + '!') :
-  console.log('Hello!')
+//if..else statement version
+let userName = 'Brandon';
+userName ? console.log(`Hello, ${userName}!`) :
+  console.log('Hello!');
 
-let userQuestion = 'is moshing the coolest?';
-  console.log(`The user asked: ${userQuestion}`);
-let randomNumber = Math.floor(Math.random() * 8); 
+let userQuestion = 'Will Trump get elected again?';
+console.log(`${userName} asked "${userQuestion}"`);
+
+let randomNumber = Math.floor(Math.random() *8);
 let eightBall = '';
-switch (randomNumber) {
-  case 0:
-    eightBall = 'It is god damn certain';
-    break;
-  case 1:
-    eightBall = 'It is decidely so!';
-    break;
-  case 2:
-   eightBall ='Hell yea they are';
-    break;
-  case 3:
-    eightBall = 'How dare you ask such a blasphemous question.';
-    break;
-  case 4:
-   eightBall = 'You can set your watch to it';
-    break;
-  case 5:
-    eightBall = 'My sources say YEA';
-    break;
-  case 6:
-    eightBall = 'Outlook is so god damn rad';
-    break;
-  case 7:
-    eightBall = 'Signs point to yes';
-    break;
+if (randomNumber = 0) {
+  console.log('It is certain');
+} else if (randomNumber = 1) {
+  console.log ('It is decidely so');
+} else if (randomNumber = 2) {
+  console.log ('Reply hazy try again');
+} else if (randomNumber = 3) {
+  console.log('cannot predict now');
+} else if (randomNumber = 4) {
+  console.log('Do not count on it');
+} else if (randomNumber = 5) {
+  console.log('My sources say no');
+} else if (randomNumber = 6) {
+  console.log('Outlook not so good');
+} else if (randomNumber = 7) {
+  console.log('Signs point to yes');
 }
-console.log(`${eightBall}`);
+console.log(eightBall)
+
+
 
 
 
